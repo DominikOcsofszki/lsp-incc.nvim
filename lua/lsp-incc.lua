c27336426411b400c1af199d79ff8a73b3c18e32
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-01-02 15:12:27 +0100

    incc-lsp plugin - init

---
+++ lua/lsp-incc.lua
@@ -0,0 +1,65 @@
+-- local LSP_IMPORTS  = require("lsp-incc-nvim.lsp-all-import")
+
+local M            = {}
+local CMD_INCC24   = "/Users/dominik/HOME/BA/DEV/MAIN/src/incc_lsp/SERVER.sh"
+local DEFAULT_OPTS = {
+	path = CMD_INCC24,
+	CAPABILITIES = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	ON_ATTACH = M.ON_ATTACH,
+	SETTINGS = {}
+}
+
+local function merge_options(conf)
+	return vim.tbl_deep_extend("force", DEFAULT_OPTS, conf or {})
+end
+local create_autocmd_ft_incc   = function()
+	vim.filetype.add({
+		extension = {
+			incc = 'incc',
+			incc24 = 'incc',
+		},
+	})
+end
+
+local create_incc24_lsp_config = function(conf)
+	local merged_config = merge_options(conf)
+	if not require 'lspconfig.configs'.incc24_lsp then
+		require 'lspconfig.configs'.incc24_lsp = {
+			default_config = {
+				cmd = { merged_config.path },
+				filetypes = { 'tx', 'incc', 'incc24' },
+				root_dir = function(fname)
+					return require 'lspconfig'.util.find_git_ancestor(fname)
+				end,
+				settings = merged_config.SETTINGS,
+				on_attach = merged_config.ON_ATTACH,
+				capabilities = merged_config.CAPABILITIES,
+			},
+		}
+	end
+	require("lspconfig").incc24_lsp.setup {}
+end
+-- local create_incc24_lsp_config = function()
+-- 	if not require 'lspconfig.configs'.incc24_lsp then
+-- 		require 'lspconfig.configs'.incc24_lsp = {
+-- 			default_config = {
+-- 				cmd = { M.configs.CMD_INCC24 },
+-- 				filetypes = { 'tx', 'incc', 'incc24' },
+-- 				root_dir = function(fname)
+-- 					return require 'lspconfig'.util.find_git_ancestor(fname)
+-- 				end,
+-- 				settings = {},
+-- 				on_attach = LSP_IMPORTS.ON_ATTACH,
+-- 				capabilities = LSP_IMPORTS.CAPABILITIES,
+-- 			},
+-- 		}
+-- 	end
+-- 	require("lspconfig").incc24_lsp.setup {}
+-- end
+
+
+M.setup = function(conf)
+	create_autocmd_ft_incc()
+	create_incc24_lsp_config(conf)
+end
+return M
---
+++ plugin/lsp-incc.lua
@@ -0,0 +1,7 @@
+local lsp_incc = require("lsp-incc")
+
+local M = {}
+M.setup = function(conf)
+	lsp_incc.setup(conf)
+end
+return M
