7e13e2397b8523905fe5283f98c0ecaba0077474
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-01-19 08:57:53 +0100

    incc v0.2

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # python3 -m pip install lsp/incc_lsp-0.1.0-py3-none-any.whl
-pipx install lsp/incc_lsp-0.1.0-py3-none-any.whl
+# pipx install lsp/incc_lsp-0.1.0-py3-none-any.whl
+pipx install lsp/incc_lsp-0.2.0-py3-none-any.whl
